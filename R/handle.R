78077c8018a441cf466fd73974ef9d2f0643e925
alexvpickering <alexvpickering@gmail.com>
2018-01-16 16:46:38 -0800

    rework POST based on opencpu; implement JWT
    Instead of using rApache POST variable, uses receiveBin from
    rApache to get POST body as raw. This allows for customized
    parsing of POST body based on content-type.
    
    JSON Web Tokens implementation allows for restricting API
    endpoints to users who log in. After log-in, user is given
    jwt encoded with a secret. Subsequent API calls validate
    the jwt (sent in request 'Authorization'header) usng the
    same secret and run R function only if valid.

--- R/handle.R
+++ R/handle.R
@@ -1,61 +1,113 @@
 
 #' Handles incomming rApache requests
 #'
-#' @param GET rApache variable: list of parameters
-#' @param POST rApache variable: list of parameters
-#' @param COOKIES rApache variable: list of cookies
-#' @param FILES rApache variable:
-#' @param SERVER rApache variable: list
-#' @param allowed_packages character vector of allowed packages
-#' @param timeout maximum time in seconds to allow for call to return
-#' @param rlimits named vector/list with RLIMIT values, for example: c(cpu = 60, fsize = 1e6)
+#' @param SERVER rApache variable.
+#' @param packages Vector of packages that api allows.
+#' @param open Endpoints that are open to non-authenticated users.
+#'    Specify as list of length 2 character vectors
+#'    for example: list(c('package_name', 'package_function'))
+#' @param key string or raw vector needed to decode JSON web token.
+#' @param timeout Time limit in seconds for function to execute.
+#' @param rlimits named vector/list with rlimit values,
+#'    for example: c(cpu = 60, fsize = 1e6).
+
+#' @return
+#' @export
+#'
+#' @examples
+handle <- function(SERVER,
+                   packages,
+                   open = 'all',
+                   secret = NULL,
+                   timeout = 0,
+                   rlimits = NULL) {
+
+  # lowercase headers
+  req_headers <- SERVER$headers_in
+  names(req_headers) <- tolower(names(req_headers))
+
+  endpoint <- get_endpoint(SERVER)
+
+  # check if endpoint is allowed/exported
+  if(!endpoint_valid(endpoint, packages)) return()
+
+  # get claim if endpoint requires JSON web token
+  claim <- if(!endpoint_open(endpoint, open)) {
+    validate_jwt(req_headers, secret)
+  }
+
+
+    # POST body to raw
+    post <- if (SERVER$method == 'POST') {
+      list(
+        body  = rapache('receiveBin'),
+        ctype = req_headers[['content-type']]
+      )
+    }
+
+
+  #
+  # # get function to call (same as pkg::name)
+  # what <- getExportedValue(endpoint[1], endpoint[2])
+  #
+  # # call function in forked process with specified limits
+  # result <- sys::eval_safe(
+  #   do.call(what, params),
+  #   timeout = timeout,
+  #   rlimits = rlimits
+  # )
+  #
+  # # return JSON object
+  # set_status(200L)
+  # cat(as.vector(jsonlite::toJSON(result)))
+}
+
+#' Validates JSON web token
+#'
+#' Error occurs if validation fails.
+#'
+#' @param req_headers List with request headers.
+#' @inheritParams handle
+#'
+#' @return List with claim.
+#' @export
+#'
+#' @examples
+validate_jwt <- function(req_headers, secret) {
+
+  if (is.null(secret))
+    stop("argument 'secret' must be a string or raw vector")
+
+  jwt <- req_headers[['authorization']]
+  jwt <- gsub('^Bearer ', '', jwt)
+
+  jose::jwt_decode_hmac(jwt, secret)
+}
+
+
+endpoint_open <- function(endpoint, open='all') {
+  if (open == 'all') return(TRUE)
+  any(sapply(open, function(x) identical(x, endpoint)))
+}
+
+
+#' Helper function to permit testing.
+#'
+#' Calls rApache function only if rapache environment
+#'
+#' @param rapache_function
+#' @param arguments
 #'
 #' @return
 #' @export
 #'
 #' @examples
-handle <- function(GET, POST, COOKIES, FILES, SERVER,
-                   allowed_packages, timeout = 0, rlimits = NULL) {
-
-  params   <- get_params(GET, POST, SERVER)
-  endpoint <- get_endpoint(SERVER)
-
-  if (!valid_endpoint(endpoint, allowed_packages)) return()
-
-  # get function to call (same as pkg::name)
-  what <- getExportedValue(endpoint[1], endpoint[2])
-
-  # call function in forked process with specified limits
-  result <- sys::eval_safe(
-    do.call(what, params),
-    timeout = timeout,
-    rlimits = rlimits
-  )
-
-  # return JSON object
-  set_status(200L)
-  as.vector(jsonlite::toJSON(result))
-
+rapache <- function(rapache_function, arguments=list()) {
+  if (is.environment('rapache'))
+    do.call(rapache_function, arguments)
 }
 
 
-#'
-#'
-#' Helper function to permit testing. Instead of setting status directly,
-#' checks if environment is rApache and only calls setStatus if so.
-#'
-#' @param status
-#' @param message
-#'
-#' @return
-#'
-#' @examples
-set_status <- function(status, message=NULL) {
-  if (is_rapache()) {
-    setStatus(status=status)
-    cat(message)
-  }
-}
 
 #' Check if endpoint is allowed/exists
 #'
@@ -65,13 +117,14 @@ set_status <- function(status, message=NULL) {
 #' @return
 #'
 #' @examples
-valid_endpoint <- function(endpoint, allowed_packages) {
+endpoint_valid <- function(endpoint, packages) {
+
 
   # check if package is allowed
-  if (!endpoint[1] %in% allowed_packages) {
+  if (!endpoint[1] %in% packages) {
 
-    message <- paste('Package', endpoint[1], 'is not an allowed package.\n')
-    set_status(403L, message)
+    cat('Package', endpoint[1], 'is not an allowed package\n')
+    rapache('setStatus', list(status=403L))
     return(FALSE)
   }
 
@@ -79,8 +132,8 @@ valid_endpoint <- function(endpoint, allowed_packages) {
   package_functions <- getNamespaceExports(endpoint[1])
   if (!endpoint[2] %in% package_functions) {
 
-    message <- paste0('Function ', endpoint[2], ' is not exported by package ', endpoint[1], '.\n')
-    set_status(404L, message)
+    cat('Function', endpoint[2], 'is not exported by package', endpoint[1], '\n')
+    rapache('setStatus', list(status=404L))
     return(FALSE)
   }
 
@@ -88,36 +141,6 @@ valid_endpoint <- function(endpoint, allowed_packages) {
 }
 
 
-
-#' Checks if rApache is being used
-#'
-#' @return
-#' @keywords internal
-#'
-#' @examples
-is_rapache <- function() {
-  if (exists('setStatus')) return(TRUE)
-  return(FALSE)
-}
-
-
-#' Get parameters from API call
-#'
-#' @param GET rApache variable: list of parameters
-#' @param POST rApache variable: list of parameters
-#' @param SERVER rApache variable: list
-#'
-#' @return params named list
-#' @keywords internal
-#'
-#' @examples
-get_params <- function(GET, POST, SERVER) {
-
-  params <- switch(SERVER$method,
-                   'GET' = GET,
-                   'POST' = POST)
-  return(params)
-}
 
 #' Gets the package and function names from a path
 #'
--- tests/testthat/test_handle.R
+++ tests/testthat/test_handle.R
@@ -2,20 +2,7 @@
 library(handlr)
 
 SERVER <- list(method='GET')
-POST <- list(a=1, b=2)
-GET <- list(a=3, b=4)
 
-
-test_that("get_params return params equal to POST or GET", {
-
-  # GET request
-  expect_equal(GET, handlr:::get_params(GET, POST, SERVER))
-
-  # POST request
-  SERVER$method <- 'POST'
-  expect_equal(POST, handlr:::get_params(GET, POST, SERVER))
-
-})
 
 test_that("get_endpoint returns vector of package name and function", {
 
@@ -31,26 +18,60 @@ test_that("get_endpoint returns vector of package name and function", {
 
 })
 
-test_that("valid_endpoint allows exported functions in specified packages", {
+test_that("endpoint_valid allows exported functions in specified packages", {
 
-    result <- handlr:::valid_endpoint(c('ggplot2', 'ggplot'), 'ggplot2')
+    result <- handlr:::endpoint_valid(c('ggplot2', 'ggplot'), 'ggplot2')
     expect_true(result)
 
-    result <- handlr:::valid_endpoint(c('ggplot2', 'matrix'), 'ggplot2')
+    result <- handlr:::endpoint_valid(c('ggplot2', 'matrix'), 'ggplot2')
     expect_false(result)
 
-    result <- handlr:::valid_endpoint(c('base', 'matrix'), 'base')
+    result <- handlr:::endpoint_valid(c('base', 'matrix'), 'base')
     expect_true(result)
 
-    result <- handlr:::valid_endpoint(c('gobledigook', 'matrix'), 'base')
+    result <- handlr:::endpoint_valid(c('gobledigook', 'matrix'), 'base')
     expect_false(result)
 
     # example test that should fail but doesn't
     # not critical as handlr functions will not be endpoints
-    result <- handlr:::valid_endpoint(c('handlr', 'valid_endpoint'), 'handlr')
+    result <- handlr:::endpoint_valid(c('handlr', 'endpoint_valid'), 'handlr')
     expect_failure(expect_false(result))
 })
 
-test_that("set_status returns NULL outside of rApache", {
-  expect_null(handlr:::set_status(404L, 'Error!'))
+test_that("endpoint_open checks for jwt requirement", {
+
+  endpoint <- c('ggplot2', 'ggplot')
+  result <- handlr:::endpoint_open(endpoint, list(endpoint))
+  expect_true(result)
+
+  result <- handlr:::endpoint_open(endpoint, list(c('ggplot2', 'aes')))
+  expect_false(result)
+
+  result <- handlr:::endpoint_open(endpoint, list(c('ggplot2', NA)))
+  expect_false(result)
 })
+
+
+test_that("validate_jwt returns claim or throws error if invalid secret", {
+
+  claim <- jose::jwt_claim(user = "alex")
+  secret <- 'real_secret'
+  jwt <- jose::jwt_encode_hmac(claim, secret)
+  req_headers <- list('authorization' = paste('Bearer', jwt))
+
+  result <- handlr:::validate_jwt(req_headers, secret)
+  expect_equal(claim, result)
+
+  # fake secret
+  expect_error(handlr:::validate_jwt(req_headers, 'fake_secret'))
+
+  # wrong jwt
+  req_headers$authorization <- 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ'
+  expect_error(handlr:::validate_jwt(req_headers, secret))
+
+
+
+
+
+})
+
